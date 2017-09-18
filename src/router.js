5cffea04a3d1885be40e9a9d57b1836d39f7a932
Landon <landonwjohnson@gmail.com>
2017-09-18 10:25:48 -0600

    Finished Step 2

--- src/router.js
+++ src/router.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import {Switch, Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 
-import Cart from './components/Cart/Car';
+import Cart from './components/Cart/Cart';
 import Details from './components/Details/Details';
-import Landing from './components/Landing/Landon';
+import Landing from './components/Landing/Landing';
 import Shop from './components/Shop/Shop';
 
 
